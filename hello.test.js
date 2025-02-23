1bf50a9262d679ed31560dc4ed8bba8e7d0a973e
reyyan <reyyankoc@gmail.com>
2025-02-22 19:14:39 -0600

    update the hello message

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
 const hello = require("./hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Hello World from Texas test RKK");
+    expect(hello.hello()).toEqual("Hello World RKK");
   });
 });
